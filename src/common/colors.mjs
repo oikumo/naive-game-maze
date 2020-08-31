b498d0e7a457361137756d979cd545b440d5c8dc
oikumo <oikumo@gmail.com>
2020-08-30 20:49:28 -0400

    Updated Renderer test

--- src/common/colors.mjs
+++ src/common/colors.mjs
@@ -1,9 +1,11 @@
 const white = 0xFF000000
 const red = 0xFF0000FF
 const black = 0xFFFFFFFF
+const blue = 0xFFFF0000
 
 export {
     white,
     red,
-    black
+    black,
+    blue
 }
--- tests/game/renderer-test.mjs
+++ tests/game/renderer-test.mjs
@@ -1,8 +1,8 @@
 import { test, assertions } from 'naive-tests'
 import { RenderTexture } from 'naive-3d'
 import { Renderer } from '../../src/game/renderer.mjs'
-import { red, white } from '../../src/common/colors.mjs'
-const { sameArrayElements } = assertions
+import { red, white, blue, black } from '../../src/common/colors.mjs'
+const { sameArrayElements, equals } = assertions
 
 test('renderer create', () => {
     const bytesPerPixel = 4
@@ -30,4 +30,40 @@ test('renderer clear', () => {
 
     const expected = new Uint32Array([red, red, red, red])
     sameArrayElements(expected, renderer.renderTex.texture)
+})
+
+test('renderer draw and paint', () => {
+    const pixelsSize = 4
+    const bytesPerPixel = 4
+    const renderTexture = new RenderTexture(pixelsSize * bytesPerPixel)
+
+    const canvas = {
+        buffer: [],
+        draw: function (buffer) {
+            this.buffer = buffer
+        }
+    }
+
+    const renderer = new Renderer(canvas, renderTexture)
+
+    renderTexture.texture[0] = white
+    renderTexture.texture[1] = red
+    renderTexture.texture[2] = blue
+    renderTexture.texture[3] = black
+
+    const brush = (texture) => {
+        texture[0] = red
+        texture[1] = red
+        texture[2] = red
+        texture[3] = red
+    }
+    renderer.paint(brush)
+    renderer.draw()
+
+    for (let i = 0; i < canvas.buffer.length; i += 4) {
+        equals(255, canvas.buffer[i + 0])
+        equals(0, canvas.buffer[i + 1])
+        equals(0, canvas.buffer[i + 2])
+        equals(255, canvas.buffer[i + 3])
+    }
 })
