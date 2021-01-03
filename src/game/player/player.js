26f5898f0529e3d4e231fffa8f7d609ec0ec4b7a
oikumo <oikumo@gmail.com>
2021-01-03 19:01:20 -0300

    Refactor

--- src/game/player/player.js
+++ src/game/player/player.js
@@ -1,10 +1,16 @@
 import { vector3Forward, vector3Zero } from '../../common/math/vector/vector3.js'
+import { PlayerAvatar } from './player-avatar.js';
 
 export function Player() {
     this.position = vector3Zero();
     this.direction = vector3Forward();
+    this.avatar = new PlayerAvatar(50, 50);
 }
 
 Player.prototype.moveTo = function (target) {
     this.position = target;
+}
+
+Player.prototype.draw = function (renderer) {
+    this.avatar.draw(this.position, renderer.tex(), renderer.texWidth());
 }
