8b464aeda98cc7145bcf077dd680d36e68e90b4c
oikumo <oikumo@gmail.com>
2021-01-24 22:14:15 -0300

    Added Circle draw on texture test

--- tests/game/geometry/primitives/circles/circle-on-texture-test.js
+++ tests/game/geometry/primitives/circles/circle-on-texture-test.js
@@ -1,9 +1,40 @@
 import { test, assertions } from 'naive-tests';
-import { red } from '../../../../../src/common/colors.js';
+import { red, white } from '../../../../../src/common/colors.js';
 import { createTexture } from '../../../../../src/engine/tex.js';
+import { drawCircleOnTexture } from '../../../../../src/game/geometry/primitives/circles/circle-on-texture.js';
 const { equals } = assertions;
 
-test('circle create', () => {
-    const tex = createTexture(50, 50, red);
-    tex.fill(() => 0);
+test('circle on texture - create', () => {
+    const tex = createTexture(50, 50, white);
+    drawCircleOnTexture(tex, red, 0.99);
+
+    const pixelRadiusMaxError = 2;
+    const len = tex.width * tex.height;
+    const centerX = Math.floor(tex.width * 0.5);
+    const centerY = Math.floor(tex.height * 0.5);
+    const pixels = tex.pixels;
+    const radius = centerX;
+
+    let x = 0;
+    let y = 0;
+    let error = 0;
+    let distance = 0;
+    let acceptableRadiusLen = true;
+
+    for (let i = 0; i < len; i++) {
+
+        if (pixels[i] === red) {
+            distance = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2));
+            error = Math.abs(distance - radius);
+            acceptableRadiusLen = error < pixelRadiusMaxError;
+            equals(acceptableRadiusLen, true, `Circle not draw properly at pixel x: ${x} y: ${y} err: ${error}`);
+        }
+
+        if (x + 1 === tex.width) {
+            y++;
+            x = 0;
+        } else {
+            x++;
+        }
+    }
 });
