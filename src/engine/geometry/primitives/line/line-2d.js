5a32f8692481a16c0bedb9d07d543f7e07e7f6f5
oikumo <oikumo@gmail.com>
2021-03-14 20:51:42 -0300

    Added polygon, vector2

--- src/engine/geometry/primitives/line/line-2d.js
+++ src/engine/geometry/primitives/line/line-2d.js
@@ -1,20 +1,20 @@
-export function drawLine2D(tex, texWidth, texHeight, color, p1, p2, q1, q2) {
+export function drawLine2D(tex, p, q, color) {
     let i, t, x, y;
-    const delta1 = q1 - p1;
-    const delta2 = q2 - p2;
+    const delta1 = q.x - p.x;
+    const delta2 = q.y - p.y;
     const n = Math.max(Math.abs(delta1), Math.abs(delta2));
 
     for (i = n - 1; i >= 0; --i) {
         t = i / n
 
-        x = Math.floor(p1 + (t * delta1))
-        if (x < 0 || x >= texWidth)
+        x = Math.floor(p.x + (t * delta1))
+        if (x < 0 || x >= tex.width)
             continue
 
-        y = p2 + Math.floor(t * delta2)
-        if (y < 0 || y >= texHeight)
+        y = p.y + Math.floor(t * delta2)
+        if (y < 0 || y >= tex.height)
             continue
 
-        tex[Math.floor(y * texWidth) + x] = color
+        tex.pixels[Math.floor(y * tex.width) + x] = color
     }
 }
