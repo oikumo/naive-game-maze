33ca25f5f9aaf76c1699b229f68a9e16ae5cc98c
oikumo <oikumo@gmail.com>
2021-03-14 19:54:43 -0300

    Added line 2d primitive, Refactor

--- src/engine/tex.js
+++ src/engine/tex.js
@@ -5,3 +5,12 @@ export function createTexture(width, height, color) {
     tex.fill(() => color);
     return tex;
 }
+
+export function createCheckerTexture(width, height, color, otherColor, tileWidth = 10, tileHeight = 10) {
+    const tex = new Texture(width, height);
+    tex.fill((col, row) => {
+        const ratio = Math.floor(col / tileWidth) + Math.floor(row / tileHeight);
+        return ratio % 2 == 0 ? color : otherColor;
+    });
+    return tex;
+}
---
+++ tests/engine/geometry/primitives/line/line-2d-test.js
@@ -0,0 +1,8 @@
+import { test, assertions } from 'naive-tests'
+import { drawLine2D } from '../../../../../src/engine/geometry/primitives/line/line-2d.js';
+const { equals } = assertions;
+
+
+test('line 2d - draw', () => {
+    equals(false, false);
+})
